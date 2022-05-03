94f540db0804d83d2c92c02d779a76c0b41ae340
Simrat Grewal <simrats169169@gmail.com>
2022-05-03 13:31:04 -0700

    feat(server): Statically host css and js files

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const fs = require("fs")
 const app = express()
 
 async function main() {
+    app.use("/css", express.static("./public/css"))
+    app.use("/js", express.static("./public/js"))
+
     app.get("/", function(_, res) {
         const doc = fs.readFileSync("./public/html/landing.html", "utf8")
         res.send(doc)
