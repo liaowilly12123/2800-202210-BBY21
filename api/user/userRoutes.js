04c218b63fd8c19ff37801b3bc497667d35fc39b
Simrat Grewal <simrats169169@gmail.com>
2022-05-03 14:54:59 -0700

    feat(register): Add input validation for body data

--- api/user/userRoutes.js
+++ api/user/userRoutes.js
@@ -1,6 +1,35 @@
 const router = require("express").Router()
 
+// Checks if data is undefined and sends a fail message back to the client if it
+// is.
+// Returns true if data is undefined, else false
+function validate(res, data, msg) {
+    if (typeof data === 'undefined') {
+        res.fail(msg)
+        return true;
+    }
+    return false;
+}
+
 router.post("/register", function(req, res) {
+    const body = req.body
+    if (validate(res, body, "Request body is undefined")) return
+
+    const firstName = body.firstName
+    if (validate(res, firstName, "First Name is undefined")) return
+
+    const lastName = body.lastName
+    if (validate(res, lastName, "Last Name is undefined")) return
+
+    const email = body.email
+    if (validate(res, email, "Email is undefined")) return
+
+    const password = body.password
+    if (validate(res, password, "Password is undefined")) return
+
+    const userType = body.userType
+    if (validate(res, userType, "User type is undefined")) return
+
     res.success()
 })
 
