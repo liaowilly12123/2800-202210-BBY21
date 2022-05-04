24b7ebdf40f1863c3c608a39d552cfa1d0451590
Simrat Grewal <simrats169169@gmail.com>
2022-05-04 15:48:02 -0700

    feat(users): Add paginated endpoint to get all users

--- api/user/userRoutes.js
+++ api/user/userRoutes.js
@@ -96,4 +96,17 @@ router.get("/logout", function(req, res) {
     res.success("Successfully logged out.")
 })
 
+router.get("/all", async function(req, res) {
+    if (req.session.userType !== "admin") {
+        res.fail("User is not an admin")
+    }
+
+    // https://javascript.plainenglish.io/simple-pagination-with-node-js-mongoose-and-express-4942af479ab2
+    const { page = 1, limit = 10 } = req.query
+
+    const users = await User.find().limit(limit).skip((page - 1) * limit)
+
+    res.success(users)
+})
+
 module.exports = router
