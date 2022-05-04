9b5c8e3019806ebc498c4fc9f3419a093b59ca5b
Simrat Grewal <simrats169169@gmail.com>
2022-05-04 12:16:26 -0700

    feat(apiRoutes): Add tutorRoutes

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const userRoutes = require("./user/userRoutes.js")
+const tutorRoutes = require("./user/tutorRoutes.js")
 
 router.use("/user", userRoutes)
+router.use("/tutor", tutorRoutes)
 
 module.exports = router
