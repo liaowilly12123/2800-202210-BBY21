d05ffcc057f09ebb9611d159fcdea8e57a931a95
Simrat Grewal <simrats169169@gmail.com>
2022-05-05 14:41:02 -0700

    feat(dashboard): Use total number of pages to prevent empty pages

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,23 @@
 let currentPage = 1;
+let totalPages = null;
 
 async function setUsers(page) {
-    if (currentPage === 0) {
+    if (currentPage === 1) {
         document.getElementById("prev").disabled = true
     } else {
         document.getElementById("prev").disabled = false
     }
 
+    if (currentPage === totalPages) {
+        document.getElementById("next").disabled = true
+    } else {
+        document.getElementById("next").disabled = false
+    }
+
     const usersRes = await fetch(`/api/user/all?page=${page}`)
     const users = await usersRes.json()
+
+    console.log(users)
 
     if (users.success) {
         const payload = users.payload
@@ -16,8 +25,12 @@ async function setUsers(page) {
         const template = document.getElementById("userCardTemplate")
         const cardHolder = document.getElementById("cardHolder")
 
+        if (totalPages == null) {
+            totalPages = payload.totalPages
+        }
+
         cardHolder.replaceChildren([])
-        for (const user of payload) {
+        for (const user of payload.users) {
             const userCard = template.content.cloneNode(true)
             userCard.querySelector(".name").innerText = user.firstName + " " + user.lastName
             userCard.querySelector(".joinDate").innerText = user.joinDate
@@ -29,13 +42,16 @@ async function setUsers(page) {
 
 function updatePageNumber() {
     document.getElementById("pnumcur").innerText = currentPage
+    document.getElementById("pnumtotal").innerText = totalPages
 }
 
 setUsers(currentPage)
 
 document.getElementById("next").addEventListener('click', () => {
-    currentPage++;
-    setUsers(currentPage)
+    if (currentPage < totalPages) {
+        currentPage++;
+        setUsers(currentPage)
+    }
 })
 
 document.getElementById("prev").addEventListener('click', () => {
