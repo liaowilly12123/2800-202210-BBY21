2fcca4654a9e8e76c6dd22375049e957f772b4c0
Simrat Grewal <simrats169169@gmail.com>
2022-05-05 20:42:39 -0700

    feat(dashboard): Make delete button work

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -17,8 +17,6 @@ async function setUsers(page) {
     const usersRes = await fetch(`/api/user/all?page=${page}`)
     const users = await usersRes.json()
 
-    console.log(users)
-
     if (users.success) {
         const payload = users.payload
 
@@ -32,8 +30,29 @@ async function setUsers(page) {
         cardHolder.replaceChildren([])
         for (const user of payload.users) {
             const userCard = template.content.cloneNode(true)
+            userCard.querySelector(".card").id = user._id
             userCard.querySelector(".name").innerText = user.firstName + " " + user.lastName
             userCard.querySelector(".joinDate").innerText = user.joinDate
+            userCard.querySelector(".delete").addEventListener("click", async (e) => {
+                e.preventDefault()
+                const res = await fetch("/api/user/delete", {
+                    method: "DELETE",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        userId: user._id,
+                    }),
+                })
+                const resJson = await res.json()
+
+                if (resJson.success) {
+                    const currUserCard = document.getElementById(user._id)
+                    document.getElementById("cardHolder").removeChild(currUserCard)
+                    setUsers(currentPage)
+                }
+            })
             cardHolder.appendChild(userCard)
         }
         updatePageNumber()
