fc0b4419d11ccf6940d1e94812d2d85856cf35c4
Simrat Grewal <simrats169169@gmail.com>
2022-05-06 15:41:21 -0700

    fix(userRoutes): Send back userType in /info endpoint

--- public/js/landing.js
+++ public/js/landing.js
@@ -2,40 +2,40 @@
 const loginForm = document.getElementById("login");
 
 loginForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    document.getElementById("error").innerText = '';
+  document.getElementById("error").innerText = "";
 
-    const emailNode = document.getElementById("login-email");
-    const passwordNode = document.getElementById("login-password");
+  const emailNode = document.getElementById("login-email");
+  const passwordNode = document.getElementById("login-password");
 
-    const res = await fetch("/api/user/login", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: emailNode.value,
-            password: passwordNode.value,
-        }),
-    });
-    const responseJson = await res.json();
+  const res = await fetch("/api/user/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: emailNode.value,
+      password: passwordNode.value,
+    }),
+  });
+  const responseJson = await res.json();
 
-    if (responseJson.success) {
-        if (responseJson.payload.userType == "admin") {
-            window.location.href = "/dashboard";
-        } else {
-            window.location.href = "/profile";
-        }
+  if (responseJson.success) {
+    if (responseJson.payload.userType == "admin") {
+      window.location.href = "/dashboard";
     } else {
-        document.getElementById("error").innerText = responseJson.payload;
+      window.location.href = "/profile";
     }
+  } else {
+    document.getElementById("error").innerText = responseJson.payload;
+  }
 });
 
 function clearError() {
-    document.getElementById("error").innerText = '';
+  document.getElementById("error").innerText = "";
 }
 
-document.getElementById("login-email").addEventListener("input", clearError)
-document.getElementById("login-password").addEventListener("input", clearError)
+document.getElementById("login-email").addEventListener("input", clearError);
+document.getElementById("login-password").addEventListener("input", clearError);
