b7077738a81e0c66761bf38c582034e4456145a4
Simrat Grewal <simrats169169@gmail.com>
2022-05-11 13:58:45 -0700

    feat(models/image): Add user_id as a field

--- models/image.js
+++ models/image.js
@@ -1,14 +1,20 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 var imageSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'BBY_21_user',
+    required: true,
+  },
   name: String,
   desc: String,
   img: {
     data: Buffer,
     contentType: String,
+    required: true,
   },
 });
 
 //Image is a model which has a schema imageSchema
 
-module.exports = new mongoose.model("Image", imageSchema);
+module.exports = new mongoose.model('Image', imageSchema);
