c4b27f8273361f51dfac6bd9b159b92bd218ca35
Willy Liao <willyliao0425@gmail.com>
2022-05-11 11:27:45 -0700

    updated variable name for qualificationRoute for clarity, and removed unused import from tutors.js

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -1,7 +1,8 @@
 "use strict";
 const router = require("express").Router();
 const userRoutes = require("./user/userRoutes.js");
-const userRoutes1 = require("./qualifications/tutors.js");
+const qualificationRoutes = require("./qualifications/tutors.js");
+
 router.use("/user", userRoutes);
-router.use("/qualifications", userRoutes1);
+router.use("/qualifications", qualificationRoutes);
 module.exports = router;
