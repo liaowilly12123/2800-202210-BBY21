120a358309e11eccd0dfb87bfdb8c6e4c90bdf1b
Aodhan Cearnaigh <acearnaigh@my.bcit.ca>
2022-05-12 15:09:12 -0700

    added profile picture adddition and modification features

---
+++ models/profilePicture.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var imageSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BBY_21_user",
+    required: true,
+  },
+  img: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BBY_21_Image",
+    required: true,
+  },
+});
+
+// Image is a model which has a schema imageSchema
+module.exports = new mongoose.model("BBY_21_Profile_Image", imageSchema);
--- public/js/profile.js
+++ public/js/profile.js
@@ -13,6 +13,47 @@ async function asyncMain() {
     document.getElementById('email').innerText = payload.email;
     document.getElementById('type').innerText = payload.userType;
     document.getElementById('joinDate').innerText = payload.joinDate;
+    
+    const form = document.getElementById("profilePictureUpload");
+    const inputFile = document.getElementById("uploadButton");
+
+    const formData = new FormData();
+
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      // for(const file of inputFile.files) {
+        formData.append("myImage", inputFile.files[0]);
+      // }
+      
+      const response = await fetch("/api/user/uploadphoto", {
+        method: "post", 
+        body: formData,
+      })
+      const resJSON = await response.json();
+
+      if (resJSON.success) {
+        const response2 = await fetch("/api/user/uploadProfilePicture", {
+          method: "post",
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ imgId: resJSON.payload.id}),
+        })
+        console.log(await response2.json());
+      }
+      document.getElementById("profileImage").src=resJSON.payload.path;
+    })
+
+    const profilepic = await fetch("/api/user/profilePicture");
+    const profileJSON = await profilepic.json();
+    if (profileJSON.success) {
+      document.getElementById("profileImage").src=profileJSON.payload.path;
+    } else {
+      console.log(profileJSON)
+    }
+
   } else {
     window.location.href = '/';
   }
