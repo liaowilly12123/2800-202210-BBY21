4a060a37e811b2e20db9ee32f19f87849812d30e
Simrat Grewal <simrats169169@gmail.com>
2022-05-12 10:24:08 -0700

    feat(nav): Add dropdown on hamburger press on mobile

--- public/js/template.js
+++ public/js/template.js
@@ -3,6 +3,31 @@ async function setNav() {
   const resHtml = await res.text();
 
   document.getElementById('nav-holder').innerHTML = resHtml;
+
+  document.getElementById('burger').addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = document.getElementById('modal');
+    modal.style.display = 'unset';
+
+    const dropdown = document.getElementById('dropdown');
+    dropdown.style.top = 75 + 'px';
+  });
+
+  function hideNav() {
+    const dropdown = document.getElementById('modal');
+    const tint = document.getElementById('tint');
+
+    dropdown.classList.add('fadeOutTop');
+    tint.classList.add('fadeOut');
+
+    setTimeout(() => {
+      dropdown.classList.remove('fadeOutTop');
+      tint.classList.remove('fadeOut');
+      dropdown.style.display = 'none';
+    }, 300);
+  }
+
+  document.getElementById('tint').addEventListener('click', hideNav);
 }
 
 setNav();
