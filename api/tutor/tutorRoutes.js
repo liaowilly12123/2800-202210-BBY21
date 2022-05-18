928c8cf240c01e53a4f3eac35338a032113e4673
Willy Liao <willyliao0425@gmail.com>
2022-05-17 23:33:55 -0700

    GET /tutor/all retrieves all tutor info limited by 20 at a time

--- api/tutor/tutorRoutes.js
+++ api/tutor/tutorRoutes.js
@@ -1,6 +1,7 @@
 "use strict";
 const router = require("express").Router();
 const Tutor = require("../../models/Tutor.js");
+const User = require("../../models/User.js");
 
 // Checks if data is undefined and sends a fail message back to the client if it
 // is.
@@ -13,7 +14,7 @@ function validate(res, data, msg) {
   return false;
 }
 
-router.put("/info", async function (req, res) {
+router.put('/info', async function (req, res) {
   if (!req.session.loggedIn) {
     return res.fail("User not logged in");
   }
@@ -24,10 +25,7 @@ router.put("/info", async function (req, res) {
     );
   }
 
-  /**
-   * Finds tutor document by user ID and updates if found, else creates
-   * a new document.
-   */
+  // Finds tutor document by user ID and updates if found, else create a new document.
   Tutor.findOneAndUpdate(
     { user_id: req.session.userId },
     { ...removeEmpty(req.body) },
@@ -46,15 +44,32 @@ router.put("/info", async function (req, res) {
   );
 });
 
+router.get("/all", async function (req, res) {
+  if (!req.session.loggedIn) {
+    return res.fail("User not logged in");
+  }
+
+  // https://javascript.plainenglish.io/simple-pagination-with-node-js-mongoose-and-express-4942af479ab2
+  const { page = 1, limit = 20 } = req.query;
+
+  // https://stackoverflow.com/questions/26691543/return-certain-fields-with-populate-from-mongoose  
+  const tutors = await Tutor.find()
+    .populate('user_id', 'firstName lastName')
+    .limit(limit)
+    .skip((page - 1) * limit);
+
+  const totalPages = Math.ceil((await Tutor.count()) / limit);
+
+  return res.success({ tutors: tutors, totalPages: totalPages });
+});
+
 /**
  * Removes undefined, null, and empty values
  * https://stackoverflow.com/questions/286141/remove-blank-attributes-from-an-object-in-javascript
  */
 function removeEmpty(obj) {
   return Object.fromEntries(
-    Object.entries(obj).filter(
-      ([_, value]) => value != null && value != ""
-    )
+    Object.entries(obj).filter(([_, value]) => value != null && value != "")
   );
 }
 module.exports = router;
