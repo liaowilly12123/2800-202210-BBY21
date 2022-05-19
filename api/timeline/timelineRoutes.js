f932bd5743192c16488f10aea42e1b074b355a84
Simrat Grewal <simrats169169@gmail.com>
2022-05-19 13:50:01 -0700

    profile page now connects to the timeline endpoint and dynamically creates timeline posts if the user has posts

--- api/timeline/timelineRoutes.js
+++ api/timeline/timelineRoutes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const validate = require('../../utils/validationUtils.js');
 const Timeline = require('../../models/Timeline.js');
 
+router.get('/posts', async function (req, res) {
+  const { user_id } = req.query;
+
+  const timelinePosts = await Timeline.find({ user_id: user_id }).sort([
+    ['date', 'desc'],
+  ]);
+
+  res.success({ posts: timelinePosts });
+});
+
 router.post('/new', async function (req, res) {
   if (!req.session.loggedIn) {
     return res.fail('User not logged in');
