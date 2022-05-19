096734574b995a65ed4edb967b85a64855487af9
sim <simrats169169@gmail.com>
2022-05-19 13:35:44 -0700

    Revert "Simrat timeline create post"

--- models/Timeline.js
+++ models/Timeline.js
@@ -1,30 +1,26 @@
-'use strict';
-const mongoose = require('mongoose');
+"use strict";
+const mongoose = require("mongoose");
 
 const timelineSchema = new mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'BBY_21_user',
-    required: true,
-  },
-  heading: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
+    ref: "BBY_21_user",
     required: true,
   },
   date: {
     type: Date,
-    required: true,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
   },
   img: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'BBY_21_Image',
     required: true,
-  },
+  }
 });
 
-const timelineModel = mongoose.model('BBY_21_timeline', timelineSchema);
+const timelineModel = mongoose.model("BBY_21_timeline", timelineSchema);
 module.exports = timelineModel;
