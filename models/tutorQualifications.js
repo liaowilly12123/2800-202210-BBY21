563a5387b5c18e1e38e7b484ab0b8f92aa909307
Nimratcheema03 <nimratcheema03@gmail.com>
2022-05-18 19:10:23 -0700

    removed some buggy code

--- models/tutorQualifications.js
+++ models/tutorQualifications.js
@@ -1,24 +1,37 @@
-"use strict";
+"use strict ";
 const mongoose = require("mongoose");
 
 const tutorQualificationsSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "BBY_21_user",
-    required: true,
-  },
-  higherEducation: {
-    type: String,
-    required: true,
-  },
-  experience: {
-    type: String,
-    required: true,
-  },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "BBY_21_users",
+        required: true,
+    },
+    higherEducation: {
+        type: String,
+        required: true,
+    },
+    experience: {
+        type: String,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    pay: {
+        type: Number,
+
+    }
 });
 
 const tutorQualificationsModel = mongoose.model(
-  "BBY21_tutor_qualifications",
-  tutorQualificationsSchema
+    "BBY21_tutor_qualifications",
+    tutorQualificationsSchema
 );
 module.exports = tutorQualificationsModel;
--- public/js/tutors.js
+++ public/js/tutors.js
@@ -1,78 +1,40 @@
 'use strict';
 
 const registerForm = document.getElementById('tutor');
-// const submit = document.getElementById('Submit');
-// submit.onclick = function () {
-//   document.getElementById('tutor').submit();
-//   // document.getElementById('form2').submit();
-// };
-registerForm.addEventListener('submit', async (e) => {
-  console.log('bruh');
-  e.preventDefault();
-  const educationNode = document.getElementById('tutor-education');
-  const experienceNode = document.getElementById('tutor-experience');
-  const FirstnameNode = document.getElementById('tutor-name');
-  const LastnameNode = document.getElementById('tutor-name1');
+const button = document.getElementById('buttons')
+registerForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const educationNode = document.getElementById('tutor-education');
+    const experienceNode = document.getElementById('tutor-experience');
+    const contact = document.getElementById('tutor-contact');
+    const subject = document.getElementById('tutor-subject');
+    const info = document.getElementById('tutor-info');
 
-  const res = await fetch('/api/tutor/qualifications', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      higherEducation: educationNode.value,
-      experience: experienceNode.value,
-      firstName: FirstnameNode.value,
-      lastName: LastnameNode.value,
-    }),
-  });
 
-  const responseJson = await res.json();
-  if (responseJson.success) {
-    console.log('Registered Succesfully');
-  } else {
-    console.log(responseJson);
-    console.error('problem in registering');
-  }
+    const res = await fetch('/api/tutor/qualifications', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            higherEducation: educationNode.value,
+            experience: experienceNode.value,
+            contactNumber: contact.value,
+            subject: subject.value,
+            pay: info.value,
+        }),
+    });
+
+    const responseJson = await res.json();
+    if (responseJson.success) {
+        console.log('Registered Succesfully');
+        window.location.href = "/upload";
+    } else {
+        console.log(responseJson);
+        console.error('problem in registering');
+    }
 });
-
-// const registerForm = document.getElementById('tutor');
-// const button = document.getElementById('buttons')
-// registerForm.addEventListener('submit', async(e) => {
-//     console.log('bruh');
-//     e.preventDefault();
-//     const educationNode = document.getElementById('tutor-education');
-//     const experienceNode = document.getElementById('tutor-experience');
-//     const contact = document.getElementById('tutor-contact');
-//     const subject = document.getElementById('tutor-subject');
-//     const info = document.getElementById('tutor-info');
-
-
-//     const res = await fetch('/api/tutor/qualifications', {
-//         method: 'POST',
-//         headers: {
-//             Accept: 'application/json',
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//             higherEducation: educationNode.value,
-//             experience: experienceNode.value,
-//             contactNumber: contact.value,
-//             subject: subject.value,
-//             pay: info.value,
-//         }),
-//     });
-
-//     const responseJson = await res.json();
-//     if (responseJson.success) {
-//         console.log('Registered Succesfully');
-//         window.location.href = "/upload";
-//     } else {
-//         console.log(responseJson);
-//         console.error('problem in registering');
-//     }
-// });
 // button.addEventListener("click", async (e) => {
 //     e.preventDefault();
 //     const res = await fetch("/api/user/delete1", {
@@ -121,25 +83,24 @@ function setProfileData(payload) {
     hideModal();
    
     const payload = userInfo.payload;
-    console.log(payload);
   
     // Set info on profile
     setProfileData(payload);
     document.getElementById('editButton').addEventListener('click', showModal);
     document.getElementById('editTint').addEventListener('click', hideModal);
-    document.getElementById('edit-fname').placeholder = payload.higherEducation;
-    document.getElementById('edit-lname').placeholder = payload.experience;
-    document.getElementById('edit-email').placeholder = payload.contactNumber;
-    document.getElementById('edit-password').placeholder = 'Password';
-    document.getElementById('edit-passwor').placeholder = payload.hourlyPay;
+    document.getElementById('edit-education').placeholder = payload.higherEducation;
+    document.getElementById('edit-experience').placeholder = payload.experience;
+    document.getElementById('edit-contact').placeholder = payload.contactNumber;
+    document.getElementById('edit-subject').placeholder = payload.subject;
+    document.getElementById('edit-pay').placeholder = payload.hourlyPay;
     // Other edits
     document.getElementById('editForm').addEventListener('submit', async (e) => {
       e.preventDefault();
-    const firstNameElem = document.getElementById('edit-fname');
-    const lastNameElem = document.getElementById('edit-lname');
-    const emailElem = document.getElementById('edit-email');
-    const passwordElem = document.getElementById('edit-password');
-    const passwordElemt = document.getElementById('edit-passwor');
+    const education = document.getElementById('edit-education');
+    const experience = document.getElementById('edit-experience');
+    const contact = document.getElementById('edit-contact');
+    const sub = document.getElementById('edit-subject');
+    const pay = document.getElementById('edit-pay');
     const res = await fetch(`/api/user/info1?id=${userId}`, {
       method: 'PUT',
       headers: {
@@ -148,19 +109,16 @@ function setProfileData(payload) {
       },
       body: JSON.stringify({
         payload: removeEmpty({
-          higherEducation: firstNameElem.value || null,
-          experience: lastNameElem.value || null,
-          contactNumber: emailElem.value || null,
-          subject: passwordElem.value || null,
-          hourlyPay : passwordElemt.value|| null,
+          higherEducation: education.value || null,
+          experience: experience.value || null,
+          contactNumber: contact.value || null,
+          subject: sub.value || null,
+          hourlyPay : pay.value|| null,
         }),
       })
       })
-      console.log(firstNameElem.value);
     const responseJson = await res.json();
-    console.log(responseJson);
-    console.log(responseJson.payload.higherEducation);
-    console.log(responseJson.payload.experience);
+
     if (responseJson.success) {
       setProfileData(responseJson.payload);
       hideModal();
