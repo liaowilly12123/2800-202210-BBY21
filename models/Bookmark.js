3c94b666bd41a99aa144a7a83e2e616a8da2c9b6
Willy Liao <willyliao0425@gmail.com>
2022-05-24 13:48:25 -0700

    created a Bookmark schema model

---
+++ models/Bookmark.js
@@ -0,0 +1,20 @@
+"use strict";
+const mongoose = require("mongoose");
+
+const bookmarksSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "BBY_21_users",
+    required: true,
+    unique: true,
+  },
+  bookmarks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BBY_21_users",
+    },
+  ],
+});
+
+const bookmarksModel = mongoose.model("BBY_21_bookmarks", bookmarksSchema);
+module.exports = bookmarksModel;
