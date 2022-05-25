11c3e0076a715bfbb0b113ee428a8f860cb58dbb
Nimratcheema03 <nimratcheema03@gmail.com>
2022-05-25 15:41:48 -0700

    allow tutors to enter multiple subjects in the form

--- public/js/tutors.js
+++ public/js/tutors.js
@@ -1,29 +1,45 @@
 'use strict';
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("tutorModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+var submitbtn = document.getElementById("submit");
 btn.onclick = function() {
   modal.style.display = "block";
 }
 span.onclick = function() {
   modal.style.display = "none";
 }
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+submitbtn.onclick = function() {
+  modal.style.display = "none";
 }
+document.getElementById('tutor-subject').addEventListener('keyup', addTopic);
 
+let searchTopics = [];
+
+ function addTopic(e) {
+  e.preventDefault();
+  const topics = document.getElementById('tutor-subject');
+  if (e.key !== 'Enter') {
+    return;
+  }
+  if (searchTopics.includes(topics.value)) {
+    return;
+  }
+  searchTopics.push(topics.value);
+  const topicPillContainer = document.getElementById('topicPillContainer');
+  const newPill = document.createElement('div');
+  newPill.innerHTML = topics.value;
+  newPill.className = 'newPill';
+  topicPillContainer.appendChild(newPill);
+  topics.value = '';
+}
+const educationNode = document.getElementById('tutor-education');
+const experienceNode = document.getElementById('tutor-experience');
+const contact = document.getElementById('tutor-contact');
+const info = document.getElementById('tutor-info');
 const registerForm = document.getElementById('tutor');
 registerForm.addEventListener('submit', async(e) => {
     e.preventDefault();
-    const educationNode = document.getElementById('tutor-education');
-    const experienceNode = document.getElementById('tutor-experience');
-    const contact = document.getElementById('tutor-contact');
-    const subject = document.getElementById('tutor-subject');
-    const info = document.getElementById('tutor-info');
-
-
     const res = await fetch('/api/tutor/info', {
         method: 'PUT',
         headers: {
@@ -34,16 +50,15 @@ registerForm.addEventListener('submit', async(e) => {
             higherEducation: educationNode.value,
             experience: experienceNode.value,
             contact: contact.value,
-            topics: subject.value,
+            topics: searchTopics,
             pricing: info.value,
         }),
     });
-    modal.style.display = "none";
     const responseJson = await res.json();
     if (responseJson.success) {
-        console.log('Registered Succesfully');
+        console.log('updated Succesfully');
     } else {
         console.log(responseJson);
-        console.error('problem in registering');
+        console.error('problem in updating');
     }
 });
