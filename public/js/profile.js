49b05d96fcd82da15d8219f937a28d53bbed77bd
Nimratcheema03 <nimratcheema03@gmail.com>
2022-05-25 11:47:41 -0700

    added tutors qualification form on profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -228,6 +228,5 @@ function removeEmpty(obj) {
 
 
 
-
   
 
--- public/js/tutors.js
+++ public/js/tutors.js
@@ -1,37 +1,49 @@
 'use strict';
-const registerForm = document.getElementById('tutor');
-// const submit = document.getElementById('Submit');
-// submit.onclick = function () {
-//   document.getElementById('tutor').submit();
-//   // document.getElementById('form2').submit();
-// };
-registerForm.addEventListener('submit', async (e) => {
-  console.log('bruh');
-  e.preventDefault();
-  const educationNode = document.getElementById('tutor-education');
-  const experienceNode = document.getElementById('tutor-experience');
-  const FirstnameNode = document.getElementById('tutor-name');
-  const LastnameNode = document.getElementById('tutor-name1');
-
-  const res = await fetch('/api/tutor/qualifications', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      higherEducation: educationNode.value,
-      experience: experienceNode.value,
-      firstName: FirstnameNode.value,
-      lastName: LastnameNode.value,
-    }),
-  });
-
-  const responseJson = await res.json();
-  if (responseJson.success) {
-    console.log('Registered Succesfully');
-  } else {
-    console.log(responseJson);
-    console.error('problem in registering');
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
+
+const registerForm = document.getElementById('tutor');
+registerForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const educationNode = document.getElementById('tutor-education');
+    const experienceNode = document.getElementById('tutor-experience');
+    const contact = document.getElementById('tutor-contact');
+    const subject = document.getElementById('tutor-subject');
+    const info = document.getElementById('tutor-info');
+
+
+    const res = await fetch('/api/tutor/info', {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            higherEducation: educationNode.value,
+            experience: experienceNode.value,
+            contact: contact.value,
+            topics: subject.value,
+            pricing: info.value,
+        }),
+    });
+    modal.style.display = "none";
+    const responseJson = await res.json();
+    if (responseJson.success) {
+        console.log('Registered Succesfully');
+    } else {
+        console.log(responseJson);
+        console.error('problem in registering');
+    }
 });
