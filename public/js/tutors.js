fc00ef23c0074d6bb4555586c191d3b9d95fc360
Simrat Grewal <simrats169169@gmail.com>
2022-05-25 17:01:42 -0700

    feat: Rework tutor qualifications to merge with current app

--- public/js/tutors.js
+++ public/js/tutors.js
@@ -1,22 +1,21 @@
 'use strict';
-var modal = document.getElementById("tutorModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-var submitbtn = document.getElementById("submit");
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-submitbtn.onclick = function() {
-  modal.style.display = "none";
-}
+import Modal from '/js/modal/modal.js';
+import { showToast } from '/js/toast.js';
+
+const qualificationsModal = new Modal(
+  'tutorQualifications',
+  document.getElementById('tutorQualificationsForm')
+);
+
+document
+  .getElementById('addQualificationsButton')
+  .addEventListener('click', () => qualificationsModal.show());
+
 document.getElementById('tutor-subject').addEventListener('keyup', addTopic);
 
 let searchTopics = [];
 
- function addTopic(e) {
+function addTopic(e) {
   e.preventDefault();
   const topics = document.getElementById('tutor-subject');
   if (e.key !== 'Enter') {
@@ -33,32 +32,33 @@ let searchTopics = [];
   topicPillContainer.appendChild(newPill);
   topics.value = '';
 }
+
 const educationNode = document.getElementById('tutor-education');
 const experienceNode = document.getElementById('tutor-experience');
 const contact = document.getElementById('tutor-contact');
 const info = document.getElementById('tutor-info');
-const registerForm = document.getElementById('tutor');
-registerForm.addEventListener('submit', async(e) => {
-    e.preventDefault();
-    const res = await fetch('/api/tutor/info', {
-        method: 'PUT',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            higherEducation: educationNode.value,
-            experience: experienceNode.value,
-            contact: contact.value,
-            topics: searchTopics,
-            pricing: info.value,
-        }),
-    });
-    const responseJson = await res.json();
-    if (responseJson.success) {
-        console.log('updated Succesfully');
-    } else {
-        console.log(responseJson);
-        console.error('problem in updating');
-    }
+const registerForm = document.getElementById('tutorQualificationsForm');
+
+registerForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const res = await fetch('/api/tutor/info', {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      higherEducation: educationNode.value,
+      experience: experienceNode.value,
+      contact: contact.value,
+      topics: searchTopics,
+      pricing: info.value,
+    }),
+  });
+  const responseJson = await res.json();
+  if (responseJson.success) {
+    showToast('success', 'Information Updated');
+  } else {
+    showToast('error', responseJson.payload);
+  }
 });
